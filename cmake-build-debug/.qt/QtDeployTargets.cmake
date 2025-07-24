166bf4679bff3a2536cd9dc092fb7e3427854bac
01-Dreamer <2711339704@qq.com>
2025-07-24 22:14:45 +0800

    test

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_chatclient_FILE D:/AllData/CLion/chatclient/cmake-build-debug/chatclient.exe)
+set(__QT_DEPLOY_TARGET_chatclient_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_chatclient_RUNTIME_DLLS D:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
